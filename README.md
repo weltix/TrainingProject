c6f80cc26e6adea6c47c314745486485910c169e
DMITRIY BLUDOV <weltix@gmail.com>
2018-09-18 12:06:49 +0300

    [Вася] добавлен подвал

--- README.md
+++ README.md
@@ -1,7 +1,4 @@
 # TrainingProject
 Project for training my java programming skills
 
-This file specially changed to make Pull request
-147
-125
-p0009i
+[Вася] добавлен подвал
--- src/First.java
+++ src/First.java
@@ -1,2 +1,21 @@
+/*
+ * Copyright (c) 07.09.2018, DMITRIY BLUDOV. All rights reserved.
+ */
+
+import java.time.Clock;
+import java.time.Instant;
+
 public class First {
+    public static void main(String[] args) {
+
+        System.out.println(System.currentTimeMillis());
+
+        Instant instant = Clock.systemDefaultZone().instant();
+
+        System.out.println(instant.getEpochSecond());
+        System.out.println(instant.getNano());
+
+        System.out.println(instant.toEpochMilli());
+
+    }
 }
